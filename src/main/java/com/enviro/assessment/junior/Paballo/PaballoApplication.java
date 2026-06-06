d1a619bbed209c8ec16802ffbe5790293a971a5c
Paballo <paballokunutu@gmail.com>
2026-06-06 20:53:11 +0200

    Initial project setup: Spring Boot + H2 + dependencies

---
+++ src/main/java/com/enviro/assessment/junior/Paballo/PaballoApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.Paballo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaballoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaballoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/junior/Paballo/PaballoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.junior.Paballo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaballoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
