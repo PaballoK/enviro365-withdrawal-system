5fb0ba20b3c143ffbf7c04ee283889caa02331da
Paballo <paballokunutu@gmail.com>
2026-06-08 14:02:01 +0200

    feat: wire up app shell with routes and HttpClient

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },   
+
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./views/dashboard/dashboard.component').then((m) => m.DashboardComponent),
+  },
+  {
+    path: 'withdrawal',
+    loadComponent: () =>
+      import('./views/withdrawal/withdrawal.component').then((m) => m.WithdrawalComponent),
+  },
+  {
+    path: 'history/:id',
+    loadComponent: () => 
+      import('./views/history/history.component').then((m) => m.HistoryComponent),
+  },
+  { path: '**', redirectTo: 'dashboard' },
+];
